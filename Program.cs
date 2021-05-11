483f720d3d97d12266c60ac0bf89395b159e0916
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-11 22:20:37 +0530

    Instantiating delegate and handeling event

--- Program.cs
+++ Program.cs
@@ -7,38 +7,50 @@ namespace EventDelegatePractice1
     { 
         static void Main(string[] args)
         {
-            SchoolHierarchyHandler sc1 = new SchoolHierarchyHandler(SchoolHandler1);
-            SchoolHierarchyHandler sc2 = new SchoolHierarchyHandler(SchoolHandler2);
-            SchoolHierarchyHandler sc3 = new SchoolHierarchyHandler(SchoolHandler3);
+            //SchoolHierarchyHandler sc1 = new SchoolHierarchyHandler(SchoolHandler1);
+            //SchoolHierarchyHandler sc2 = new SchoolHierarchyHandler(SchoolHandler2);
+            //SchoolHierarchyHandler sc3 = new SchoolHierarchyHandler(SchoolHandler3);
 
-            //sc1(2, SchoolType.Nursary);
-            //sc2(1, SchoolType.Primary);
+            ////sc1(2, SchoolType.Nursary);
+            ////sc2(1, SchoolType.Primary);
 
-            sc1 += sc2 + sc3;
-            int finalYears = sc1(10, SchoolType.Secondary);
-            Console.WriteLine(finalYears);
+            //sc1 += sc2 + sc3;
+            //int finalYears = sc1(10, SchoolType.Secondary);
+            //Console.WriteLine(finalYears);
+
+            Students student = new Students();
+            student.StudentPassed += new SchoolHierarchyHandler(student_StudentPassed);
+            student.DoScoolLevel(8, SchoolType.Secondary);
+
         }
-        static void SchoolLevel(SchoolHierarchyHandler sc)
+
+        static int student_StudentPassed(int years, SchoolType schoolType)
         {
-            sc(1, SchoolType.Nursary);
-            sc(2, SchoolType.Primary);
-            sc(10, SchoolType.Secondary);
-            sc(2, SchoolType.HigherEducation);
-        }
-        static int SchoolHandler1(int years, SchoolType schoolType)
-        {
-            Console.WriteLine("SchoolHandler1 called "+schoolType+" "+years);
+            Console.WriteLine(years+" "+schoolType);
             return years;
         }
-        static int SchoolHandler2(int years, SchoolType schoolType)
-        {
-            Console.WriteLine("SchoolHandler2 called " + schoolType + " " + years);
-            return years;
-        }
-        static int SchoolHandler3(int years, SchoolType schoolType)
-        {
-            Console.WriteLine("SchoolHandler3 called " + schoolType + " " + years);
-            return years+3;
-        }
+
+        //static void SchoolLevel(SchoolHierarchyHandler sc)
+        //{
+        //    sc(1, SchoolType.Nursary);
+        //    sc(2, SchoolType.Primary);
+        //    sc(10, SchoolType.Secondary);
+        //    sc(2, SchoolType.HigherEducation);
+        //}
+        //static int SchoolHandler1(int years, SchoolType schoolType)
+        //{
+        //    Console.WriteLine("SchoolHandler1 called "+schoolType+" "+years);
+        //    return years;
+        //}
+        //static int SchoolHandler2(int years, SchoolType schoolType)
+        //{
+        //    Console.WriteLine("SchoolHandler2 called " + schoolType + " " + years);
+        //    return years;
+        //}
+        //static int SchoolHandler3(int years, SchoolType schoolType)
+        //{
+        //    Console.WriteLine("SchoolHandler3 called " + schoolType + " " + years);
+        //    return years+3;
+        //}
     }
 }
